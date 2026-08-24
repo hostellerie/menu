f37757de7327081a65adbe345da6682f85119ca2
Ben <hostellerie.org@gmail.com>
2026-08-24 17:53:00 +0200

    Test self-contained element editor state logic

--- tests/element_editor.php
+++ tests/element_editor.php
@@ -11,46 +11,64 @@ function menu_editor_assert($condition, $message)
 }
 
 $root = dirname(__DIR__);
-$js = file_get_contents($root . '/admin/js/element-editor.js');
 $create = file_get_contents($root . '/templates/default/createelement.thtml');
 $edit = file_get_contents($root . '/templates/default/editelement.thtml');
 
-menu_editor_assert($js !== false, 'element editor JavaScript missing');
 menu_editor_assert($create !== false, 'create element template missing');
 menu_editor_assert($edit !== false, 'edit element template missing');
 
-$expected = array(
-    "case '1'" => "$('#urldiv').show();",
-    "case '2'" => "$('#glfunc').show();",
-    "case '3'" => "$('#glcorediv').show();",
-    "case '4'" => "$('#plugin').show();",
-    "case '5'" => "$('#staticpage').show();",
-    "case '6'" => "$('#urldiv, #targetdiv').show();",
-    "case '7'" => "$('#phpdiv').show();",
-    "case '8'" => 'break;',
-    "case '9'" => "$('#topic').show();",
+$expectedCreate = array(
+    "case '1'" => "jQuery('#urldiv').show();",
+    "case '2'" => "jQuery('#glfunc').show();",
+    "case '3'" => "jQuery('#glcorediv').show();",
+    "case '4'" => "jQuery('#plugin').show();",
+    "case '5'" => "jQuery('#staticpage-wrapper').show();",
+    "case '6'" => "jQuery('#urldiv,#targetdiv').show();",
+    "case '7'" => "jQuery('#phpdiv').show();",
+    "case '9'" => "jQuery('#topic-wrapper').show();",
 );
 
-foreach ($expected as $case => $action) {
-    $casePos = strpos($js, $case);
-    menu_editor_assert($casePos !== false, 'missing editor mapping for ' . $case);
-    $nextCase = strpos($js, 'case ', $casePos + strlen($case));
-    $segment = $nextCase === false ? substr($js, $casePos) : substr($js, $casePos, $nextCase - $casePos);
-    menu_editor_assert(strpos($segment, $action) !== false, 'wrong visible fields for ' . $case);
+$expectedEdit = array(
+    "case '1'" => "jQuery('#urldiv').show();",
+    "case '2'" => "jQuery('#glfunc').show();",
+    "case '3'" => "jQuery('#glcorediv').show();",
+    "case '4'" => "jQuery('#plugin').show();",
+    "case '5'" => "jQuery('#staticpage').show();",
+    "case '6'" => "jQuery('#urldiv,#targetdiv').show();",
+    "case '7'" => "jQuery('#phpdiv').show();",
+    "case '9'" => "jQuery('#topic').show();",
+);
+
+foreach ($expectedCreate as $case => $action) {
+    $casePos = strpos($create, $case);
+    menu_editor_assert($casePos !== false, 'missing create mapping for ' . $case);
+    $nextCase = strpos($create, 'case ', $casePos + strlen($case));
+    $segment = $nextCase === false ? substr($create, $casePos) : substr($create, $casePos, $nextCase - $casePos);
+    menu_editor_assert(strpos($segment, $action) !== false, 'wrong create visible fields for ' . $case);
+}
+
+foreach ($expectedEdit as $case => $action) {
+    $casePos = strpos($edit, $case);
+    menu_editor_assert($casePos !== false, 'missing edit mapping for ' . $case);
+    $nextCase = strpos($edit, 'case ', $casePos + strlen($case));
+    $segment = $nextCase === false ? substr($edit, $casePos) : substr($edit, $casePos, $nextCase - $casePos);
+    menu_editor_assert(strpos($segment, $action) !== false, 'wrong edit visible fields for ' . $case);
 }
 
 foreach (array($create, $edit) as $template) {
-    menu_editor_assert(strpos($template, '/plugins/menu/js/element-editor.js') !== false, 'shared editor script not loaded');
-    menu_editor_assert(strpos($template, 'MENUElementEditor.init()') !== false, 'editor not initialized on page load');
-    menu_editor_assert(strpos($template, 'MENUElementEditor.syncFields()') !== false, 'editor not synchronized after AJAX type refresh');
+    menu_editor_assert(strpos($template, 'function syncElementFields()') !== false, 'inline element field synchronizer missing');
+    menu_editor_assert(strpos($template, "String(jQuery('#menutype').val() || '')") !== false, 'field synchronizer must read actual selected type');
+    menu_editor_assert(strpos($template, 'style="display:none;"') !== false, 'conditional fields must be hidden by default');
+    menu_editor_assert(strpos($template, 'syncElementFields();') !== false, 'initial field synchronization missing');
+    menu_editor_assert(strpos($template, 'getJSON(') !== false, 'authoritative type refresh missing');
 }
 
 menu_editor_assert(strpos($create, 'for="pluginname"') !== false, 'create plugin label must target plugin selector');
 menu_editor_assert(strpos($edit, 'for="pluginname"') !== false, 'edit plugin label must target plugin selector');
 menu_editor_assert(strpos($edit, 'for="glfunction"') !== false, 'Geeklog Action label must target glfunction selector');
-menu_editor_assert(strpos($edit, "save.attr('disabled', 'disabled')") !== false, 'edit save must be blocked before authoritative types load');
-menu_editor_assert(strpos($edit, "save.removeAttr('disabled')") !== false, 'edit save must be enabled after authoritative types load');
-menu_editor_assert(strpos($edit, '.fail(function ()') !== false, 'edit type loading failure must be handled');
-menu_editor_assert(strpos($edit, 'Reload this page before saving.') !== false, 'edit type loading failure must explain recovery');
+menu_editor_assert(strpos($edit, 'disabled="disabled"') !== false, 'edit save must be blocked before authoritative types load');
+menu_editor_assert(strpos($edit, "saveButton.removeAttr('disabled')") !== false, 'edit save must be enabled after authoritative types load');
+menu_editor_assert(strpos($create, '/plugins/menu/js/element-editor.js') === false, 'create must not depend on external editor JavaScript');
+menu_editor_assert(strpos($edit, '/plugins/menu/js/element-editor.js') === false, 'edit must not depend on external editor JavaScript');
 
 echo "Element editor UI contract tests passed" . PHP_EOL;
