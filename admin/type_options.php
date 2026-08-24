c3d967c85525c8ffca98fbcab7e216bbfce4e4f9
Ben <hostellerie.org@gmail.com>
2026-08-24 18:34:07 +0200

    Make element type endpoint independent of menu cache

--- admin/type_options.php
+++ admin/type_options.php
@@ -13,22 +13,40 @@ require_once $_CONF['path'] . 'plugins/menu/element_types.php';
 $menuId = isset($_GET['menu']) ? (int) $_GET['menu'] : 0;
 $mid = isset($_GET['mid']) ? (int) $_GET['mid'] : 0;
 
-if ($menuId <= 0 || !isset($Menus[$menuId])) {
+if ($menuId <= 0) {
     header('HTTP/1.1 404 Not Found');
     exit;
 }
 
+$menuType = DB_getItem($_TABLES['menu'], 'menu_type', 'id=' . $menuId);
+if ($menuType === false || $menuType === null || $menuType === '') {
+    header('HTTP/1.1 404 Not Found');
+    exit;
+}
+$menuType = (int) $menuType;
+
 $currentType = null;
 $locked = false;
-if ($mid > 0 && isset($Menus[$menuId]['elements'][$mid])) {
-    $currentType = (int) $Menus[$menuId]['elements'][$mid]->type;
+if ($mid > 0) {
+    $currentTypeValue = DB_getItem(
+        $_TABLES['menu_elements'],
+        'element_type',
+        'id=' . $mid . ' AND menu_id=' . $menuId
+    );
+
+    if ($currentTypeValue === false || $currentTypeValue === null || $currentTypeValue === '') {
+        header('HTTP/1.1 404 Not Found');
+        exit;
+    }
+
+    $currentType = (int) $currentTypeValue;
     $locked = ($currentType === 1);
 }
 
-$hasStaticPages = isset($_PLUGINS) && in_array('staticpages', $_PLUGINS);
+$hasStaticPages = isset($_PLUGINS) && in_array('staticpages', $_PLUGINS, true);
 $types = MENU_getAllowedElementTypes(
     $LANG_MENU_TYPES,
-    (int) $Menus[$menuId]['menu_type'],
+    $menuType,
     $hasStaticPages,
     $currentType
 );
