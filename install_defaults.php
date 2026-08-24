51d731080f3d0c7dfbef8fbd7ee2d62f538296c0
Ben <hostellerie.org@gmail.com>
2026-08-24 21:46:39 +0200

    Keep Menu global configuration compatible across Geeklog versions

--- install_defaults.php
+++ install_defaults.php
@@ -54,20 +54,24 @@ function MENU_addConfig130($c, $includeStructure)
     if ($includeStructure) {
         $c->add('sg_main', null, 'subgroup', 0, 0, null, 0, true, $me, 0);
         $c->add('tab_main', null, 'tab', 0, 0, null, 0, true, $me, 0);
-        $c->add('fs_runtime', null, 'fieldset', 0, 0, null, 0, true, $me, 0);
-        $c->add('fs_security', null, 'fieldset', 0, 0, null, 100, true, $me, 0);
-        $c->add('fs_compatibility', null, 'fieldset', 0, 0, null, 200, true, $me, 0);
-        $c->add('fs_diagnostics', null, 'fieldset', 0, 0, null, 300, true, $me, 0);
+        $c->add('fs_main', null, 'fieldset', 0, 0, null, 0, true, $me, 0);
     }
 
     // selectionArray 0 is Geeklog's standard Yes / No selector.
+    // Runtime and accessibility.
     $c->add('enable_cache', $defaults['enable_cache'], 'select', 0, 0, 0, 10, true, $me, 0);
     $c->add('accessibility_markup', $defaults['accessibility_markup'], 'select', 0, 0, 0, 20, true, $me, 0);
+
+    // Security.
     $c->add('external_link_protection', $defaults['external_link_protection'], 'select', 0, 0, 0, 110, true, $me, 0);
     $c->add('allow_php_elements', $defaults['allow_php_elements'], 'select', 0, 0, 0, 120, true, $me, 0);
+
+    // Compatibility / legacy presentation.
     $c->add('legacy_rendering', $defaults['legacy_rendering'], 'select', 0, 0, 0, 210, true, $me, 0);
     $c->add('load_legacy_css', $defaults['load_legacy_css'], 'select', 0, 0, 0, 220, true, $me, 0);
     $c->add('load_legacy_js', $defaults['load_legacy_js'], 'select', 0, 0, 0, 230, true, $me, 0);
+
+    // Diagnostics.
     $c->add('debug', $defaults['debug'], 'select', 0, 0, 0, 310, true, $me, 0);
 }
 
