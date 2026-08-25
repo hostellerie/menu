08f362f53f03901f3a4e3bd211a56dd5e62d0505
Ben <hostellerie.org@gmail.com>
2026-08-25 13:49:50 +0200

    Align tooltip contract with anchored CSS tooltip

--- tests/admin_tree_ui_contract.php
+++ tests/admin_tree_ui_contract.php
@@ -2,6 +2,7 @@
 
 $root = dirname(__DIR__);
 $class = file_get_contents($root . '/classes/classMenuElement.php');
+$template = file_get_contents($root . '/templates/default/menutree.thtml');
 
 if (strpos($class, "plugins/menu/images/info.png") === false) {
     fwrite(STDERR, "Element info icon missing\n");
@@ -9,15 +10,27 @@ if (strpos($class, "plugins/menu/images/info.png") === false) {
 }
 
 $infoPos = strpos($class, "plugins/menu/images/info.png");
-$context = substr($class, max(0, $infoPos - 500), 1200);
+$context = substr($class, max(0, $infoPos - 500), 1500);
 
 if (strpos($context, 'COM_getTooltip') !== false) {
     fwrite(STDERR, "Element info icon must not use positioned Geeklog tooltip markup\n");
     exit(1);
 }
 
-if (strpos($context, 'title=') === false) {
-    fwrite(STDERR, "Element info icon must expose a native title tooltip\n");
+if (strpos($context, 'menu-info-tooltip') === false
+    || strpos($context, 'menu-info-tooltip-text') === false) {
+    fwrite(STDERR, "Element info icon must expose the anchored CSS tooltip markup\n");
+    exit(1);
+}
+
+if (strpos($context, 'title=') !== false) {
+    fwrite(STDERR, "Element info icon must not fall back to delayed native title tooltip\n");
+    exit(1);
+}
+
+if (strpos($template, '.menu-info-tooltip-text') === false
+    || strpos($template, '.menu-info-tooltip:hover .menu-info-tooltip-text') === false) {
+    fwrite(STDERR, "Anchored tooltip CSS is missing from Menu tree template\n");
     exit(1);
 }
 
