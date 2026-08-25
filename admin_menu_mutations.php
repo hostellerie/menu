e9e399d4dc5e1a05b8aab3883174646f841ea2ba
Ben <hostellerie.org@gmail.com>
2026-08-25 11:39:39 +0200

    Add Menu admin mutation module

---
+++ admin_menu_mutations.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('VERSION')) {
+    die('This file can not be used on its own.');
+}
+
+/**
+ * Menu administration mutations.
+ *
+ * State-changing helpers live here so admin/index.php can remain focused on
+ * routing and page composition. Request authorization/CSRF enforcement stays
+ * centralized in admin_security.php.
+ */
