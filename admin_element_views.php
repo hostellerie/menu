883aafd4a44f0b9ea0c4ad39f00433ed5241f882
Ben <hostellerie.org@gmail.com>
2026-08-25 11:41:51 +0200

    Add Menu element admin view module

---
+++ admin_element_views.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('VERSION')) {
+    die('This file can not be used on its own.');
+}
+
+/**
+ * Menu administration element/configuration view builders.
+ *
+ * These functions compose admin UI only. State changes are implemented in
+ * admin_menu_mutations.php or dedicated endpoints.
+ */
