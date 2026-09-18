a2bcdf95fe9b6643b029277a44b41fa1b8713ba0
Ben <hostellerie.org@gmail.com>
2026-09-18 07:31:34 +0700

    Test documentation-backed configuration tooltips

--- tests/config_tooltip_contract.php
+++ tests/config_tooltip_contract.php
@@ -2,31 +2,43 @@
 
 $root = dirname(__DIR__);
 $runtime = file_get_contents($root . '/runtime_config.php');
+$documentation = file_get_contents($root . '/public_html/config.html');
 
-$required = array(
+$runtimeRequired = array(
+    'function plugin_getdocumentationurl_menu($file)',
+    "'/menu/config.html'",
     'function plugin_getconfigtooltip_menu($id)',
-    "'enable_cache' =>",
-    "'accessibility_markup' =>",
-    "'external_link_protection' =>",
-    "'allow_php_elements' =>",
-    "'legacy_rendering' =>",
-    "'load_legacy_css' =>",
-    "'load_legacy_js' =>",
-    "'debug' =>",
-    'french_france_utf-8',
-    'return isset($tooltips[$id]) ? $tooltips[$id] : \'\';',
+    'return null;',
 );
 
-foreach ($required as $needle) {
+foreach ($runtimeRequired as $needle) {
     if (strpos($runtime, $needle) === false) {
         fwrite(STDERR, "Configuration tooltip contract missing: {$needle}\n");
         exit(1);
     }
 }
 
-if (strpos($runtime, "'samplesetting1' =>") !== false
-    || strpos($runtime, "'samplesetting2' =>") !== false) {
-    fwrite(STDERR, "Obsolete sample settings must not receive tooltips\n");
+$options = array(
+    'enable_cache',
+    'accessibility_markup',
+    'external_link_protection',
+    'allow_php_elements',
+    'legacy_rendering',
+    'load_legacy_css',
+    'load_legacy_js',
+    'debug',
+);
+
+foreach ($options as $option) {
+    if (strpos($documentation, 'name="desc_' . $option . '"') === false) {
+        fwrite(STDERR, "Configuration documentation missing anchor: desc_{$option}\n");
+        exit(1);
+    }
+}
+
+if (strpos($documentation, 'samplesetting1') !== false
+    || strpos($documentation, 'samplesetting2') !== false) {
+    fwrite(STDERR, "Obsolete sample settings must not appear in configuration documentation\n");
     exit(1);
 }
 
